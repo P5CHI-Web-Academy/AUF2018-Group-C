f4b6f295144392edf000f038a7c93295dfacc2c2
Maria BUFTEA <mbuftea@pentalog.com>
2018-11-26 14:59:54 +0200

    Maria

---
+++ index.php
@@ -0,0 +1,27 @@
+<TABLE border="1" cellspacing="1" cellpadding="5">
+ <TR bgcolor="#c0c0ff">
+ <TD>Caracteristica
+ <TD>Auto
+ <TD>Train
+ <TD>Avion
+ <TR bgcolor="yellow" align="center">
+ <TD align="left"><B>Distance</B>
+ <TD>3 km
+ <TD colspan="1">6 km
+ <TD bgcolor="#ffd0d0">2 km
+ <TR bgcolor="yellow" align="center">
+ <TD align="left"><B>Temps</B>
+ <TD>
+ <TD>
+ <TD>
+ <TR bgcolor="yellow" align="center">
+ <TD align="left"><B>Prix</B>
+ <TD rowspan="1" bgcolor="#ffd0d0">
+ <TD>
+ <TD>
+ <TR bgcolor="yellow" align="center">
+ <TD align="left"><B>Disponibilite</B>
+ <TD>
+ <TD>
+ <TD>
+</TABLE> 
